1433048dac51e87ebbcb98c5a2fa1b028a5160b1
0xc0ffee <nullptr.delete@yanbdex.ru>
2022-08-22 20:44:07 +0200

    stm32f1-discovery: Added support for a new board

--- src/ClockManager.cpp
+++ src/ClockManager.cpp
@@ -4,10 +4,41 @@
 
 #include "ClockManager.h"
 
-#include "stm32f4xx_hal.h"
+#include <nice-bsp/common/nice_bsp.h>
 
 bool ClockManager::init()
 {
+#ifdef STM32F100xB
+        RCC_OscInitTypeDef RCC_OscInitStruct = {0};
+        RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
+
+        /** Initializes the RCC Oscillators according to the specified parameters
+        * in the RCC_OscInitTypeDef structure.
+        */
+        RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSI;
+        RCC_OscInitStruct.HSIState = RCC_HSI_ON;
+        RCC_OscInitStruct.HSICalibrationValue = RCC_HSICALIBRATION_DEFAULT;
+        RCC_OscInitStruct.PLL.PLLState = RCC_PLL_NONE;
+        if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
+        {
+            return false;
+        }
+
+        /** Initializes the CPU, AHB and APB buses clocks
+        */
+        RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
+                                      |RCC_CLOCKTYPE_PCLK1|RCC_CLOCKTYPE_PCLK2;
+        RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_HSI;
+        RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
+        RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV1;
+        RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV1;
+
+        if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_0) != HAL_OK)
+        {
+            return false;
+        }
+
+#elif defined(STM32F401xC)
     RCC_OscInitTypeDef RCC_OscInitStruct = { 0 };
     RCC_ClkInitTypeDef RCC_ClkInitStruct = { 0 };
 
@@ -35,6 +66,8 @@ bool ClockManager::init()
     if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_3) != HAL_OK) {
         return false;
     }
-
+#else
+#error "This device family not supported yet"
+#endif
     return true;
 }
